93cc181a1d385e0fba634705ad2f33db3584ce98
Tim Knab <knabt@metrumrg.com>
2023-10-29 21:21:22 -0400

    Updates.

--- src/PMSimulatorBase.jl
+++ src/PMSimulatorBase.jl
@@ -1,11 +1,13 @@
 module PMSimulatorBase
 using ModelingToolkit
 using SciMLBase
+using PMParameterizedBase
 abstract type PMEvent end
-
+PMModel = PMParameterizedBase.PMModel
 include("events.jl")
 include("evsFromDF.jl")
 include("helpers.jl")
+include("collect_evs.jl")
 
 export PMUpdate
 export PMInput
