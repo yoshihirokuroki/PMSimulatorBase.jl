b08e55db1a228b938cab16f52bd1f0c9f58772b8
Tim Knab <knabt@metrumrg.com>
2023-10-29 21:33:31 -0400

    Add DiffEqCallbacks to Deps

--- src/PMSimulatorBase.jl
+++ src/PMSimulatorBase.jl
@@ -2,12 +2,13 @@ module PMSimulatorBase
 using ModelingToolkit
 using SciMLBase
 using PMParameterizedBase
+using DiffEqCallbacks
 abstract type PMEvent end
 PMModel = PMParameterizedBase.PMModel
 include("events.jl")
 include("evsFromDF.jl")
 include("helpers.jl")
-includ("assemble.jl")
+include("assemble.jl")
 include("collect_evs.jl")
 
 export PMUpdate
